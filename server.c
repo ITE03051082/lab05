1cd5ee2973085b4c5a40b92b163ce74f2e8ce747
ITE03051082 <hower@hower-VirtualBox.(none)>
2016-09-26 18:15:42 +0800

    9/26

--- server.c
+++ server.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 int main(int argc, char *argv[])
 {
 int socket_desc;
 struct sockaddr_in server;
+char *message , server_reply[2000];
 socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 if (socket_desc == -1)
 {
@@ -19,5 +21,19 @@ puts ("connect error");
 return 1;
 }
 puts ("Connected");
+
+message = "GET / HTTP/1.1\r\n\r\n" ;
+if (send(socket_desc , message , strlen (message),0)<0)
+{
+puts("Send failed");
+return 1;
+}
+puts("Data Send\n");
+if(recv(socket_desc, server_reply , 2000 , 0)<0)
+{
+puts("recv failed");
+}
+puts("Reply received\n");
+puts(server_reply);
 return 0;
 }
